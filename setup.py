6f4ce50b1b164827f7d6b5497d4dc5b4952af300
danielxwang90 <daniel.wang@oliverwyman.com>
2024-02-22 21:55:27 -0500

    fixed requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('metadata.json') as fr:
 
 setuptools.setup(
     name="gryphon-distribution-fitting",  # Name of the repository
-    version="0.0.1",
+    version="0.0.12,
     author=metadata.get("author", ""),
     author_email=metadata.get("author_email", ""),
     description=metadata.get("description", ""),
